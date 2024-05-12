544266983e5a0ec57637cea36619b7d3ce4fbe38
Samuel <187swachira@gmail.com>
2024-05-11 20:03:53 -0400

    added some backend functionality

--- app/config.py
+++ app/config.py
@@ -4,3 +4,4 @@ import os
 class Config(object):
     # Property used by multiple Flask add-ons for security
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'default-key-for-devs'
+    DB_FILE = os.environ.get('DB_FILE') or 'dev.db'
--- app/routes/routes.py
+++ app/routes/routes.py
@@ -1,7 +1,48 @@
+import os 
+import sqlite3
+from datetime import datetime
 from flask import render_template, redirect, Blueprint
+from ..forms import AppointmentForm
 
 bp = Blueprint('main', __name__)
+DB_FILE = os.environ.get("DB_FILE")
 
-@bp.route('/')
+
+
+@bp.route('/', methods=['GET', 'POST'])
 def main():
-    return render_template('main.html')
+    form = AppointmentForm()
+
+    if form.validate_on_submit():
+        with sqlite3.connect(DB_FILE) as conn:
+            curs = conn.cursor()
+
+            params = {
+                'name': form.name.data,
+                'start_datetime': datetime.combine(form.start_date.data, form.start_time.data),
+                'end_datetime': datetime.combine(form.end_date.data, form.end_time.data),
+                'description': form.description.data,
+                'private': form.private.data
+            }
+
+        curs.execute(""" INSERT INTO appointments(name, start_datetime, end_datetime, description, private)
+                            VALUES(:name, :start_datetime, :end_datetime, :description, :private)
+                     """, params)
+        conn.commit()
+
+        return redirect('/')
+
+    with sqlite3.connect(DB_FILE) as conn:
+        curs = conn.cursor()
+        curs.execute('SELECT id, name, start_datetime, end_datetime FROM appointments ORDER BY start_datetime')
+        rows = curs.fetchall()
+
+        appointments = [] 
+        for row in rows:
+            appointment = list(row)
+            start_datetime = datetime.strptime(appointment[2], "%Y-%m-%d %H:%M:%S")
+            end_datetime = datetime.strptime(appointment[3], "%Y-%m-%d %H:%M:%S")
+            appointment[2] = start_datetime
+            appointment[3] = end_datetime
+            appointments.append(appointment)
+    return render_template('main.html', appointments=appointments, form=form)
